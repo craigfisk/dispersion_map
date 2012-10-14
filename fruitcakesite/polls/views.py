da37417d28a310450ad804fdc3111f735d601c15
Craig Fisk <craigfisk@picocosmos.net>
2012-10-13 23:31:40 -0700

    Started on rewrites of urls and views for polls

--- fruitcakesite/polls/views.py
+++ fruitcakesite/polls/views.py
@@ -1,5 +1,6 @@
-from django.template import Context, loader
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+#from django.template import Context, loader
+#from django.http import HttpResponse
 from polls.models import Poll
 
 def index(request):
@@ -22,6 +23,7 @@ def index(request):
     s += 'full_path: {0}<br/>'.format(request.get_full_path())
     s += 'secure: {0}<br/>'.format(str(request.is_secure()))
     """
+    """
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     t = loader.get_template('polls/index.html')
     c = Context({
@@ -29,17 +31,26 @@ def index(request):
         })
     return HttpResponse(t.render(c))
     """
+    """
     output += ', '.join([p.question for p in latest_poll_list])
     return HttpResponse(output)
     """
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list } )
 
 def detail(request, poll_id):
 
 #    import pdb; pdb.set_trace()
-    import sys, os
-    p = sys.path
-    e = os.environ
-    return HttpResponse("You're seeing results of poll %s." % poll_id)
+#    import sys, os
+#    p = sys.path
+#    e = os.environ
+#    return HttpResponse("You're seeing results of poll %s." % poll_id)
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p} )
+
 
 def results(request, poll_id):
     return HttpResponse("Results of poll %s." % poll_id)
