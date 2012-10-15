a6ed25dc06d82a8b1c2f78d3a1b1144a67c86837
Craig Fisk <craigfisk@picocosmos.net>
2012-10-14 17:14:23 -0700

    With urls factored into polls app

--- fruitcakesite/fruitcakesite/urls.py
+++ fruitcakesite/fruitcakesite/urls.py
@@ -1,22 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'fruitcakesite.views.home', name='home'),
-    # url(r'^fruitcakesite/', include('fruitcakesite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-
+    url(r'^polls/', include('polls.urls')),
 )
