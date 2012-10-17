069a997a3f28de0feb610b25f1ff6bbe88d4f2e9
Craig Fisk <craigfisk@picocosmos.net>
2012-10-16 17:41:36 -0700

    Moved templates to SITE_ROOT and used multiple os.path.dirname() layers in settings.py to get PROJECT_ROOT and SITE_ROOT correct

--- fruitcakesite/fruitcakesite/urls.py
+++ fruitcakesite/fruitcakesite/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
-from fruitcakesite.views import main_page
+from fruitcakesite.views import main_page, logout_page
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', main_page),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls')),
     url(r'^myaccount/', include('myaccount.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'} ),
 )
