865f2cd72b7262e0c43beae0a2a62bd381a5d50e
Craig Fisk <craigfisk@picocosmos.net>
2012-11-17 23:32:22 -0800

    Added the notes on Django Advanced Forms:  form_coding.py, to the project

--- fruitcakesite/form_coding.py
+++ fruitcakesite/form_coding.py
@@ -9,6 +9,7 @@ Frank Wiles
 www.codinghorror.com/blog/2008/07/spartan-programming.html
 
 traditional (standard):
+
 def my_view(request, template_name="myapp/my_form.html"):
 	if request.method=='POST':
 		form = myForm(request.POST) # form 1
@@ -21,7 +22,8 @@ def my_view(request, template_name="myapp/my_form.html"):
 
 
 recommended ("easy views" 1):
-def my_view(request, template_name="myapp/my_form.html"):
+
+    def my_view(request, template_name="myapp/my_form.html"):
 	form = MyForm(request.POST or None)
 	if form.is_valid():
 		do_x()
@@ -29,13 +31,15 @@ def my_view(request, template_name="myapp/my_form.html"):
 	return render(request, templatename, {'form', form} )
 
 generally tradition is to use ModelForm:
-class MyModelForm(forms.Form):
+
+    class MyModelForm(forms.Form):
     class Meta:
         model = MyModel
         fields = ['name']
 
 easy views + ModelForm (1):
-def my_model_edit(request, slug=slug, template_name='myapp/my_model_form.html'):
+
+    def my_model_edit(request, slug=slug, template_name='myapp/my_model_form.html'):
     mymodel = get_object_or_404(MyModel, slug=slug)
     form = MyModelForm(request.POST or None, instance=mymodel)
     if form.is_valid():
@@ -47,11 +51,36 @@ def my_model_edit(request, slug=slug, template_name='myapp/my_model_form.html'):
 
 easy views + ModelForms (2):
 using defaults in ModelForms rather than setting values in view.
+
 def my_model_edit(request, template_name='myapp/my_model_form.html'):
     form = MyModelForm(request.POST or None, instance=mymodel)
     if form.is_valid():
         mymodel.save()
         return redirect('home')
     return render(request, template_name, {'form':form, 'mymodel':mymodel} )
+
+On Request / Response:
+https://docs.djangoproject.com/en/dev/ref/request-response/
+
+Modifications:
+- Use __init__() to change required to not, etc.
+- Use form.fields dictionary to add fields to a form.
+- Use "extra" parameter to a form call: form = MyModelForm(request.POST or None, exta["fred"])
+- render() ?
+
+
+HTML5? --> django-floppyforms by Bruno Renie @brutasse
+
+import floppyforms as forms
+class ExampleForm(forms.Form):
+    username = forms.CharField(
+        label = '',
+        widget = forms.TextInput(
+            attrs = {'placeholder':'@johndoe'},
+        ),
+    )
+
+replaces normal widgets with HTML5 widgets.
+
 
 
