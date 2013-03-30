49b5f7073623a0ef2c1015c5d6096aa1ea6f6150
Craig Fisk <craigfisk@picocosmos.net>
2013-03-30 00:36:05 +0000

    Changed some residual template references to urls without quotes and to photoviewer instead of myfruitcake as next values; rebuild PIL to solve the problem of PIL not finding jpeg decoder

--- fruitcakesite/fruitcakesite/settings.py
+++ fruitcakesite/fruitcakesite/settings.py
@@ -91,7 +91,7 @@ FILE_UPLOAD_PERMISSIONS = None #default: None; numeric mode which to set newly u
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.justfruitcake.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
