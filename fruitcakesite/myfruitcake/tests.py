ed8af2a4a0b7fb31725f64c1739ad53d633cc065
Craig Fisk <craigfisk@picocosmos.net>
2013-09-26 18:32:49 -0700

    Working on myfruitcake/tests

--- fruitcakesite/myfruitcake/tests.py
+++ fruitcakesite/myfruitcake/tests.py
@@ -2,6 +2,9 @@ import datetime
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
+from django.test.utils import setup_test_environment
+setup_test_environment()
+
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
@@ -23,37 +26,10 @@ class IPAddresMethodTests(TestCase):
         self.assertEqual(city==u'Portland', True)
 
 
-class SimpleTest(TestCase):
-    def setUp(self):
-        u = User.objects.create_user("ak", "ak@abc.org", "pwd")
-
-    def content_test(self, url, values):
-        """Get content of url and test that each of items in `values` list is present."""
-        r = self.c.get(url)
-        self.assertEquals(r.status_code, 200)
-        for v in values:
-            self.assertTrue(v in r.content)
-
-    def test(self):
-        self.c = Client()
-        #self.c.login(username="ak", password="pwd")
-        self.c.get('registration/login/?next=/myfruitcake/')
-        result = self.c.login(username='lindamagee', password='Sp8rky=4242')
-        #self.content_test("/", ['Copyright 2012-2013'])
-        if result:
-            print "Success"
-            #self.content_test("/", ['<P class="main">Most shipped fruitcake (<b>click photo to send</b>):</P>'])
-        else:
-            print "Login failed"
-
-        """
-        self.content_test("/forum/", ['<a href="/forum/forum/1/">forum</a>'])
-        self.content_test("/forum/forum/1/", ['<a href="/forum/thread/1/">thread</a>', "ak - post"])
-
-        self.content_test("/forum/thread/1/", ['<div class="ttitle">thread</div>', '<span class="title">post</span>', 'body <br />', 'by ak |'])
-        r = self.c.post("/forum/new_thread/1/", {"subject": "thread2", "body": "body2"})
-        r = self.c.post("/forum/reply/2/", {"subject": "post2", "body": "body3"})
-        self.content_test("/forum/thread/2/", ['<div class="ttitle">thread2</div>',
-               '<span class="title">post2</span>', 'body2 <br />', 'body3 <br />'])
-        """
+class ExistingFruitcakeTest(TestCase):
+    def test_fruitcake_has_popup(self):
+        #user = User.objects.get(username='lindamagee')
+        f = Fruitcake.objects.get(id=9)
+        self.assertEqual(f.popup, u"Pick me! You'll really like me!")
+        #Fruitcake.objects.create(pic="pics/2ef7bcf22a6564a342f41ff827643477.jpg", uploader_id="30", dt=, times_shipped=)
 
