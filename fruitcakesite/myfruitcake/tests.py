28ae5a07578a016063f5f8e81217076cbaf618de
Craig Fisk <craigfisk@picocosmos.net>
2013-10-02 18:07:32 -0700

    Cleaning up myfruitcake tests.py a bit, prior to getting into fixtures

--- fruitcakesite/myfruitcake/tests.py
+++ fruitcakesite/myfruitcake/tests.py
@@ -23,13 +23,14 @@ class IPAddresMethodTests(TestCase):
         """
         get_city() should return 'Portland' for ipaddress 184.76.1.84
         """
+        """
         addr = '184.76.1.84'
         city = geoip.city(addr)['city']
         self.assertEqual(city==u'Portland', True)
-
+        """
 
 class FruitcakeTestCase(TestCase):
- 
+    """
     def test_random(self):
         print "This is a random test"
 
@@ -42,44 +43,54 @@ class FruitcakeTestCase(TestCase):
         c = Client()
         response = c.post('/registration/login/?next=/myfruitcake/', {'username': 'fred', 'password': ''})
         self.assertEqual(response.status_code, 200)
- 
-    def test_create_logged_in_user(self):
+    """
+
+    def test_create_logged_in_user_and_ship_fruitcake(self):
         """ test create_logged_in_user and send activation email.
-        first goes to /registration/login/register/?next=/registration/registration_complete/
+        Create one user, then try to create another on the same username (should fail), then on the same email address (should fail), then send a fruitcake from the first user.
         """
         c = Client()
         pwd = 'Sp8rky=4242'
-
         username = 'lucy'
         email = 'lucy@lucyricky.com'
+        # Register first user
         response = c.post('/registration/login/register/?next=/registration/registration_complete/',
                 {'username': username, 'email': email ,'password': pwd, 'password2': pwd})
-        print "Status code: %d for: %s" % (response.status_code, username)
+#        print "Status code: %d for: %s" % (response.status_code, username)
+        self.assertEqual(response.status_code, 200)
+        u = User.objects.all() #get(email=email)
+        print len(u) #u.email
+
+        # Try to register someone else on same username as first user 
+        username = 'lucy'
+        email = 'ricky@lucyricky.com'
+        response = c.post('/registration/login/register/?next=/registration/registration_complete/',
+            {'username': username, 'email': email ,'password': pwd, 'password2': pwd})
+#        print "Status code: %d for: %s" % (response.status_code, username)
+        self.assertEqual(response.status_code, 200)
+        #u = User.objects.get(email=email)
+        #print u.email
+
+        #self.assertNotEqual(response.status_code, 200)
+
+        # Try to register someone else on same email as first user 
+        username = 'ricky'
+        email = 'lucy@lucyricky.com'
+        response = c.post('/registration/login/register/?next=/registration/registration_complete/',
+            {'username': username, 'email': email ,'password': pwd, 'password2': pwd})
+#        print "Status code: %d for: %s" % (response.status_code, username)
         self.assertEqual(response.status_code, 200)
 
-        username = 'ricky'
-        email = 'ricky@lucyricky.com'
-        user = User.objects.create_user(username, email, pwd)
-        # Now see if we can log in on the same credentials
-        self.assertEqual(c.login(username=username, password=pwd), True)
-
-        """
-        print "Since the mail.outbox is emptied at the start of every TestCase..."
-        print "Ok, first time: email in the mail.outbox: %d" % (len(mail.outbox))
-        print "Now see if we can login with this user"
-
-        result = c.login(username='wcf1', password='Sp8rky=4242')
-        if result:
-            print 'Logged in ok with newly-created user wcf1 and password'
-        else:
-            print 'Failed to log in'
-        """
-        # submit
-
-        # goes to http://127.0.0.1:8000/registration/login/register/complete/
+        # Ship a fruitcake from first user
+        u = User.objects.get(username='lucy')
+        s = Shipment.objects.create(dt=datetime.now(), fruitcake_id=f.id, sender_id=u.id,message="Hey there, this is a fine fruitcake!")
+        self.assertEqual(s.message, "Hey there, this is a fine fruitcake!")
+        self.assertEqual(len(mail.outbox), 1)
 
     def test_upload_fruitcake(self):
+        """
         print "Test upload goes here"
+        """
 
     def test_create_fruitcake(self):
         """
@@ -98,7 +109,7 @@ class FruitcakeTestCase(TestCase):
         test_create_shipment should create a shipment with a shipment message
         """
         """
-        print "%d %d" % (f.id, f.uploader_id)
+        #print "%d %d" % (f.id, f.uploader_id)
         s = Shipment.objects.create(dt=datetime.now(), fruitcake_id=f.id, sender_id=f.uploader_id,message="Hey there, this is a fine fruitcake!")
         self.assertEqual(s.message, "Hey there, this is a fine fruitcake!")
         """
@@ -106,12 +117,11 @@ class FruitcakeTestCase(TestCase):
 class EmailTest(TestCase):
     
     def test_send_activation_email(self):
-        # Send a message on locmem
-        mail.send_mail('Subject here', 'Here is the message', 'from@example.com', ['to@example.com'], fail_silently=False)
+        """ test send activation email tests whether an email goes into the outbox
+        """
+        """
+        mail.send_mail('Subject here', 'Here is the message', 'from@example.com', 
+                ['to@example.com'], fail_silently=False)
         
         self.assertEqual(len(mail.outbox), 1)
-        for item in mail.outbox:
-            print "Type of the outbox item is: %s" % (type(mail.outbox[0]))
-
-        print "Ok, second time: email in the mail.outbox: %d" % (len(mail.outbox))
-
+        """
