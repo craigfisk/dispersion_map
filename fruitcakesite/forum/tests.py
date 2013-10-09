3bbd35f55df203fbd4ecd2812ffec0fbbf182939
Craig Fisk <craigfisk@picocosmos.net>
2013-10-08 22:49:31 -0700

    Seattle episode: learned about timeout control in webclient testing

--- fruitcakesite/forum/tests.py
+++ fruitcakesite/forum/tests.py
@@ -6,8 +6,11 @@ from django.contrib.sites.models import Site
 from forum.models import *
 
 from django.core.urlresolvers import reverse
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 class SimpleTest(TestCase):
     def setUp(self):
@@ -57,6 +60,7 @@ class ForumTest(TestCase):
         print "After posting to post, resp is: %s" % (resp)
         self.assertRedirects(resp, '/registration/login/?next=/forum/post/new_thread/10/')
 
+"""
 class PostTest(TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -68,4 +72,68 @@ class PostTest(TestCase):
 
     def tearDown(self):
         self.driver.quit
+"""
 
+class ForumSeleniumTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(ForumSeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(ForumSeleniumTests, cls).tearDownClass()
+
+    def testforum(self): 
+#        self.selenium.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        ###driver = webdriver.Firefox()
+        ###driver.get("/")    #"http://localhost:8000/"
+        #driver.get("http://127.0.0.1:8000/")
+        self.selenium.find_element_by_link_text("sign-in").click()
+        # signin_element.tag_name --> u'a'
+        # signin_element.text --> u'sign-in'
+        ###signin_elem.click()   # goes to the sign-in page
+        # where username and password are the names of the form fields to fill in:
+        elem_username = self.selenium.find_element_by_name('username')
+        elem_password = self.selenium.find_element_by_name('password')
+        # send the text to the form:
+        elem_username.send_keys('lindamagee')
+        elem_password.send_keys("Sp8rky=4242")
+        elem_password.submit()          # bubbles up to the form and submits it
+
+        # go to Forum
+        elem_forum = self.selenium.find_element_by_name('forum')
+        elem_forum.click()
+        # go to a forum
+        self.selenium.find_element_by_link_text('Fruitcake tradition').click()
+        # go to a thread
+        self.selenium.find_element_by_link_text('Holiday baking for pet birds').click()
+        # Reply to create another post in the thread
+        self.selenium.find_element_by_link_text('Reply').click()
+        # Get elem for "body" of the post, fill in some text, post it:
+        ##elem_reply_body = self.selenium.find_element_by_id('subject')
+        ##elem_reply_body.send_keys("Birds and holiday baked goods.")
+        elem_reply_body = self.selenium.find_element_by_id('body')
+        elem_reply_body.send_keys("Birds are big fans of holiday baked goods.")
+        elem_reply_body.submit()
+
+        # Return to page with choice between existing and new thread:
+        self.selenium.back()
+        self.selenium.back()
+        self.selenium.back()
+        # Click on "start new discussion"
+        self.selenium.find_element_by_id("new_topic").click()
+        # fill in the new discussion form:
+        elem_new_subject = self.selenium.find_element_by_name("subject")
+        elem_new_body = self.selenium.find_element_by_id("body")
+        elem_new_subject.send_keys("This is a test")
+        elem_new_body.send_keys("This is the body of a test")
+        elem_new_body.submit()
+        # end
+        #self.selenium.close()  # closes the window
+        self.selenium.quit()   # exits the driver and closes window
+
+
+                
