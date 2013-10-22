fd889a4ec6e6ab72f1814ad1c2aa959820ba722f
Craig Fisk <craigfisk@picocosmos.net>
2013-10-22 15:23:19 -0700

    Corrected tab titles for some of the forum templates; and experimented with adding namespaces in fruitcakesite.urls, which would be good to have them per app package, but will need to change url references in templates to match -- see python notes on what changed in 1.5

--- fruitcakesite/fruitcakesite/urls.py
+++ fruitcakesite/fruitcakesite/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', logout_page),
     url(r'^admin/', include(admin.site.urls)),
-#    url(r'^polls/', include('polls.urls')),
+#    url(r'^polls/', include('polls.urls', name='polls')),
     url(r'forum/', include('forum.urls')),
     url(r'myfruitcake/', include('myfruitcake.urls')),
 
@@ -40,7 +40,7 @@ urlpatterns = patterns('',
 )
 
 # django-registration latest (see above):
-# - How to upgrade for latest (0.8):  http://docs.b-list.org/django-registration/0.8/upgrade.html
+# - How to upgrade for latest (0.8 [meanwhile: 1.0 --> further changes?]):  http://docs.b-list.org/django-registration/0.8/upgrade.html
 # - Template examples from Matias Herranz at https://github.com/matiasherranz/scoobygalletas/downloads
 # - Tutorial:  see "Django by Example" part 3, http://lightbird.net/dbe/forum3.html (helpful but deprecated
 # registration.urls etc. model)
