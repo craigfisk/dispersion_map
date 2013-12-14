7be0fcee4a23484836b42132e5bed902aa58e109
Craig Fisk <craigfisk@picocosmos.net>
2013-12-13 22:30:56 -0800

    Believe I have straightened out routing issues; also added names of templates to help for debugging

--- fruitcakesite/myfruitcake/urls.py
+++ fruitcakesite/myfruitcake/urls.py
@@ -9,11 +9,11 @@ urlpatterns = patterns('myfruitcake.views',
     
     url(r'^myfruitcake/$', login_required(MyFruitcakeListView.as_view()), name='listview'),         #all
     url(r'^myfruitcake/(?P<pk>\d+)/$', MyFruitcakeDetailView.as_view(), name='detailview'),         #one
-    url(r'^myfruitcake/(?P<fruitcake_id>\d+)/send/$', login_required('email_fruitcake'), name='send_fruitcake'),    #send
+    url(r'^myfruitcake/(?P<fruitcake_id>\d+)/send/$', 'email_fruitcake', name='send_fruitcake'),    #send
 
     url(r'^myshipments/$', login_required(ShipmentListView.as_view()), name='shipments'),           #all
     url(r'^myshipments/(?P<pk>\d+)/$', ShipmentDetailView.as_view(), name='shipment_detail'),       #one
-    url(r'^myshipments/(?P<shipment_id>\d+)/send/$', login_required('email_fruitcake'), name='send_fruitcake'),     #send
+    url(r'^myshipments/(?P<shipment_id>\d+)/send/$', 'email_fruitcake', name='send_fruitcake'),     #send
     
    
     url(r'^upload/$', 'upload_file', name='myfruitcake_upload'),
