d8abe64250eb21cde7d2921624804fe4e199cbdf
Craig Fisk <craigfisk@picocosmos.net>
2014-05-16 15:06:12 -0700

    Checked out that justfruitcake works with python 2.7.6 (on friskyfrog) and with fruitcake20131223.sql database, as backed up off zazen (includes user activity through 12/23, which appears to be latest).

--- fruitcakesite/fruitcakesite/settings.py
+++ fruitcakesite/fruitcakesite/settings.py
@@ -13,11 +13,10 @@ else:
     FUNCTION_LOGGING=False #True
     #NOTE: switch to following IF DEBUGGING LOCALLY -- writes to console (or to file)
     # On Django email backends, see https://docs.djangoproject.com/en/1.5/topics/email/
-    #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    #EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
     #EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-    #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_FILE_PATH = '/tmp/fruitcake_shipments'
+    #EMAIL_FILE_PATH = '/tmp/fruitcake_shipments'
 
 # Django will email ADMINS about broken links 404 with DEBUG = False + the following setting, 
 # but will ignore the IGNORABLE_404_URLS.
@@ -281,6 +280,7 @@ INSTALLED_APPS = (
     'registration',
     'myfruitcake',
     'forum',
+#    'django_extensions',
 ###    'south',
 )
 
